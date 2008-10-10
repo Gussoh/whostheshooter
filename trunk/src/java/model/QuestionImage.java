769524b3d181fd3a6119e35f5393bd0139ccc979
andreas.duchen <andreas.duchen@9c89767a-92e0-11dd-9ac7-c591a085a637>
2008-10-10 09:15:58 +0000

    Updated all model objects

--- trunk/src/java/model/QuestionImage.java
+++ trunk/src/java/model/QuestionImage.java
@@ -5,10 +5,15 @@
 
 package model;
 
+import java.nio.ByteBuffer;
+
 /**
  *
  * @author Gussoh
  */
 public class QuestionImage extends GameImage {
 
+    public QuestionImage(ByteBuffer imageData, int width, int height) {
+        super(imageData, width, height);
+    }
 }
