1c709425813919cf1a69af9954513e04016be71b
danieltandz <danieltandz@9c89767a-92e0-11dd-9ac7-c591a085a637>
2008-10-15 06:18:45 +0000

    displays 1 random photo from a list of 10 most recent photos

--- trunk/src/java/model/FlickrQuestionProvider.java
+++ trunk/src/java/model/FlickrQuestionProvider.java
@@ -7,6 +7,7 @@ package model;
 import com.aetrion.flickr.Flickr;
 import com.aetrion.flickr.FlickrException;
 import com.aetrion.flickr.RequestContext;
+import com.aetrion.flickr.people.User;
 import com.aetrion.flickr.photos.Photo;
 import com.aetrion.flickr.photos.PhotoList;
 import com.aetrion.flickr.photos.PhotosInterface;
@@ -17,9 +18,11 @@ import org.xml.sax.SAXException;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
+import java.net.URL;
 import java.nio.ByteBuffer;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 /**
  *
  * @author TAND0015
@@ -27,7 +30,51 @@ import java.util.List;
 public class FlickrQuestionProvider implements QuestionProvider {
     private static RequestContext requestContext;
     public Question createQuestion() {
-        return null;
+        
+        requestContext = RequestContext.getRequestContext();
+        String apiKey = "159ced7ac09b5794485ac8dee1e4be20";
+        Flickr f = new Flickr(apiKey);
+        
+        // Set the shared secret which is used for any calls which require signing.
+        f.setSharedSecret("9c8dc7a2d4967255");
+                
+        PhotosInterface photosInterface = f.getPhotosInterface();
+        PhotoList photoList = null;
+        Photo photo;
+        byte[] resultImageAsRawBytes = null;        
+        try {
+            photoList = photosInterface.getRecent(10, 1);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(FlickrQuestionProvider.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(FlickrQuestionProvider.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (FlickrException ex) {
+            Logger.getLogger(FlickrQuestionProvider.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        Random r = new Random();
+        int randint = Math.abs(r.nextInt()) % 10;       
+        photo = (Photo)photoList.get(randint); 
+        String photoUrl = photo.getMediumUrl();
+        User photoOwner = photo.getOwner();
+        String ownerIconUrl = photoOwner.getBuddyIconUrl();
+        
+         List<Monkey> monkeys = new LinkedList<Monkey>();
+        for (int i = 0; i < 4; i++) {
+            try {
+                monkeys.add(new Monkey(new URL(ownerIconUrl)));
+            } catch (IOException ex) {
+            }
+        }       
+               
+        QuestionImage qi = null;
+        try {
+            qi = new QuestionImage(new URL(photoUrl));
+        } catch (IOException ex) {
+        }
+        return new Question(monkeys, 3, qi);       
+        //return null;
         /*
          List<Monkey> monkeys = new LinkedList<Monkey>();
         for (int i = 0; i < 4; i++) 
